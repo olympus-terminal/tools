610cc5aa5cf221f88d665fdf5a2eb6bac81f594f
David R. Nelson <32707458+olympus-terminal@users.noreply.github.com>
2024-02-20 06:36:28 +0400

    Update browse_zsh_history.sh

--- browse_zsh_history.sh
+++ browse_zsh_history.sh
@@ -1,1 +1,13 @@
-#
+#!/bin/bash
+
+echo "$HOME/.zsh_history"
+
+cat "$HOME/.zsh_history"
+
+wait 5
+
+echo "Now searching history for argv1 query"
+
+#nano "$HOME/.zsh_history"
+
+fgrep "$1" "$HOME/.zsh_history"
