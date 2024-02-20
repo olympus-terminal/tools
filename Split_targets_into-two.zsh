6ac5abbb260255298af990bc6dc6443119daf6ec
David R. Nelson <32707458+olympus-terminal@users.noreply.github.com>
2024-02-20 12:59:42 +0400

    Create Split_targets_into-two.zsh
     This script will split each file into two equal halves, adding prefixes to the new files while keeping their original extension.

---
+++ Split_targets_into-two.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+for file in "$@"; do
+    if [ -f "$file" ]; then
+        file_size=$(wc -l < "$file")
+        half_size=$((file_size / 2))
+        dir_name=$(dirname "$file")
+        base_name=$(basename "$file")
+        extension="${base_name##*.}"
+        prefix="${base_name%.*}"
+        split -l $half_size "$file" "${dir_name}/${prefix}_"
+        for split_file in "${dir_name}"/${prefix}_* ; do
+            mv "$split_file" "${split_file}.${extension}"
+        done
+    fi
+done
+
